4bb00b174084e58f2d79b49ee64a59a7948a78c9
Aaron Nojima <aanojima@mit.edu>
2015-12-08 07:25:27 +0000

    Position control allows negative numbers now

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -27,6 +27,9 @@ $(document).ready(function(){
 
 	$(".float-input").keypress(function(e){
 		if ((e.which != 46 || $(this).val().indexOf('.') != -1) && (e.which < 48 || e.which > 57) || (e.which == 46 && $(this).caret().start == 0)){
+			if (e.which == 45 && $(this).val().indexOf('-') == -1){
+				return;
+			}
 			e.preventDefault();
 		}
 	});
@@ -35,20 +38,25 @@ $(document).ready(function(){
 		if ($(this).val().indexOf('.') == 0){
 			$(this).val($(this).val().substring(1));
 		}
+		if ($(this).val().lastIndexOf('-') > 0){
+			var valid = $(this).val().replace('-', '')
+			$(this).val(valid);
+		}
 	});
 
 	$("#set-position-button").on('click', function(e){
-		var x = $("#x-position").val() || 0;
-		var y = $("#y-position").val() || 0;
-		var z = $("#z-position").val() || 0;
+		var x = isNaN($("#x-position").val()) ? 0 : $("#x-position").val();
+		var y = isNaN($("#y-position").val()) ? 0 : $("#y-position").val();
+		var z = isNaN($("#z-position").val()) ? 0 : $("#z-position").val();
+		console.log(x);
 
 		$(document).trigger("setposition", [FEET(x), FEET(y), FEET(z)]);
 	});
 
 	$("#set-velocity-button").click(function(e){
-		var x = $("#x-velocity").val() || 0;
-		var y = $("#y-velocity").val() || 0;
-		var z = $("#z-velocity").val() || 0;
+		var x = isNaN($("#x-velocity").val()) ? 0 : $("#x-velocity").val();
+		var y = isNaN($("#y-velocity").val()) ? 0 : $("#y-velocity").val();
+		var z = isNaN($("#z-velocity").val()) ? 0 : $("#z-velocity").val();
 		
 		$(document).trigger("setvelocity", [FEET(x), FEET(y), FEET(z)]);
 	});
